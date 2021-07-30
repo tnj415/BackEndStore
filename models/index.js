662e225dfbbe78bb00d8471048bb27ad0b3bf1f1
Troy Johnson <tnj8510@gmail.com>
2021-07-29 21:35:33 -0700

    a lot of progress

---
+++ models/index.js
@@ -0,0 +1,47 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsToMany(Category, {
+  through: {
+    model: Product,
+    unique: true
+  },
+  as: 'category_id',
+});
+
+// Categories have many Products
+Category.belongsToMany(Product,{
+  through: {
+    model: Product,
+    unique: true
+  },
+  as: 'category_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id',
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag,{
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }, { model: ProductTag }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: ProductTag }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const tagData = await User.create(req.body);
+    // 200 status code means the request is successful
+    res.status(200).json(tagData);
+  } catch (err) {
+    // 400 status code means the server could not understand the request
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const tagData = await Driver.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: ProductTag }],
+     // attributes: {
+        // include: [
+        //   [
+        //     // Use plain SQL to add up the total mileage
+        //     sequelize.literal(
+        //       '(SELECT SUM(mileage) FROM car WHERE car.driver_id = driver.id)'
+        //     ),
+        //     'totalMileage',
+        //   ],
+        // ],
+     // },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
